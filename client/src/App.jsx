352bcada43c709c7b8409146721e0a8bce90f301
him-rane <himanshurane6501@gmail.com>
2022-04-03 11:38:59 +0530

    SetUp React App

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  Navbar,
+  Welcome,
+  Footer,
+  Services,
+  Transactions,
+} from "./components/index";
+
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar></Navbar>
+        <Welcome></Welcome>
+      </div>
+      <Services></Services>
+      <Transactions></Transactions>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import logo from "../../images/Cryptopay.png";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full flex md:justify-center justify-between items-center p-4"></nav>
+  );
+};
+
+export default Navbar;
